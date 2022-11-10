e80f56fdc0970dc5df820c4d29a8c72ccca16b01
Your Namgit config --global user.email you@example.com <marterydfoss@hotmail.no>
2022-11-10 10:41:21 +0100

    endret join beerpong og laget script

---
+++ joinpage/joinleonardofk.js
@@ -0,0 +1,10 @@
+
+  var nextStep = document.getElementById("submit");
+
+
+
+  nextStep.addEventListener('click', function(evt){
+    evt.preventDefault();
+    document.getElementById('form_id').style.display = 'none';
+    document.getElementById('thank_you').style.display = 'block';
+})
