74a92bb1da0b689891f67f85de817b6a194a4617
ingeborggundersen <ingeborg.gundersen@live.no>
2022-11-15 19:24:49 +0100

    fikset bildekarusell

--- Fk_bilder_script.js
+++ Fk_bilder_script.js
@@ -12,7 +12,7 @@ const bildeListe = ["../img/FK/leoFK.png", "../img/FK/1.jpeg", "../img/FK/2.jpeg
 "../img/FK/13.jpg", "../img/FK/14.jpg", "../img/FK/15.jpg", "../img/FK/16.jpg", "../img/FK/17.jpg", "../img/FK/18.jpg", "../img/FK/19.jpg", "../img/FK/20.jpg", "../img/FK/21.jpg", "../img/FK/22.jpg",  "../img/FK/23.jpg", "../img/FK/24.jpg", "../img/FK/25.JPG", "../img/FK/leonardo_quote.jpg" ]
 
 
-function bildeshow() {
+/*function bildeshow() {
     //abouttext.style.display = "none"
 
     hovedbilde.src = bildeListe[0]
@@ -24,7 +24,7 @@ function bildeshow() {
     
     clickleft.style.display = "show";
     clickright.style.display = "show";
-}
+}*/
 
 hovedbilde.addEventListener("click", bildeshow)
 
@@ -32,23 +32,26 @@ let currentIndex = 0;
 let currentIndex2 = bildeListe.length;
 
 
+
 function nestebilde() {
     if (currentIndex >= bildeListe.length) {
         currentIndex = 0
     }
-        hovedbilde.src = bildeListe[currentIndex]
         currentIndex++
+        hovedbilde.src = bildeListe[currentIndex]
+        console.log(currentIndex);
     }
 
 function forrigebilde() {
     /* if (currentIndex >= bildeListe.length) {
         currentIndex = 0
     } */
-    if (currentIndex = 0) {
+    if (currentIndex == 0) {
         currentIndex = bildeListe.length
-    }
-        hovedbilde.src = bildeListe[currentIndex]
+    }   
         currentIndex--
+        hovedbilde.src = bildeListe[currentIndex]
+        console.log(currentIndex);
 }
 
 clickright.addEventListener("click", nestebilde)
