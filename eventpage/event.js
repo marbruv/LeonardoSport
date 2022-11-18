3368722448e0c8d9d5bc4e64d8904ae8347fdaa8
Maria Bruvik <maria.bruvik@icloud.com>
2022-11-19 00:23:52 +0100

    samlet alt til main.css, fikset footer

--- eventpage/event.js
+++ eventpage/event.js
@@ -20,3 +20,15 @@ function konkurranse3() {
     var popup = document.getElementById("svar3").innerHTML="Vi har trening ved IPD";
     popup.classList.toggle("show");
 }
+
+function svar1 () {
+    document.getElementById("svar1").style.display = "none";
+}
+
+function svar2 () {
+    document.getElementById("svar2").style.display = "none";
+}
+
+function svar3 () {
+    document.getElementById("svar3").style.display = "none";
+}
