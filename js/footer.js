c52c5293dda8240b6ee461a7ceda8ee3cfa48c97
Maria Bruvik <maria.bruvik@icloud.com>
2022-11-21 00:44:04 +0100

    ordnet homepage, må fikse media queries og footer^

--- js/footer.js
+++ js/footer.js
@@ -14,26 +14,30 @@ face.style.width = "100px";
 face.style.backgroundColor = "white";
 face.style.borderRadius = "50%";
 face.style.marginRight = "10px";
+face.style.opacity = "0.8";
 face.onclick = function() {
     window.location.href = 'https://www.facebook.com/leonardo.linjeforening.7';
 }
 face.onmouseover = function() {
-    face.style.opacity = "0.6";
+    face.style.opacity = "1";
+    face.style.cursor = "pointer";
 }
 face.onmouseleave = function() {
-    face.style.opacity = "1";
+    face.style.opacity = "0.8";
 }
 var insta = document.createElement("img");
 insta.src = "../img/instag.png";
 insta.style.width = "100px";
+insta.style.opacity = "0.8";
 insta.onclick = function() {
     window.location.href = 'https://www.instagram.com/leonardo.linjeforening/';
 }
 insta.onmouseover = function() {
-    insta.style.opacity = "0.6";
+    insta.style.opacity = "1";
+    insta.style.cursor = "pointer";
 }
 insta.onmouseleave = function() {
-    insta.style.opacity = "1";
+    insta.style.opacity = "0.8";
 }
 
 
@@ -41,3 +45,5 @@ imgDiv.appendChild(face);
 imgDiv.appendChild(insta);
 
 footerDiv.appendChild(imgDiv);
+
+footerDiv.style.opacity = "0.6";
